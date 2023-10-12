67b5c4c6e2da081be69f3db15f2e79aa178f9c46
Eyüp Can Esen <eyupcanee@gmail.com>
2023-10-12 19:19:54 +0300

    Deep Learning ve Data Preprocessing için gerekli kütüphaneler import edildi.

---
+++ lyrics_generator_ai.py
@@ -0,0 +1,13 @@
+# Gerekli Kütüphanelerin Import Edilmesi
+from __future__ import print_function
+import io
+import os
+import sys
+import string
+import numpy as np
+import pandas as pd
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import LambdaCallback, ModelCheckpoin, EarlyStopping
+from tensorflow.keras.layers import Dense, Dropout, Activation, LSTM, Bidirectional, Embedding
